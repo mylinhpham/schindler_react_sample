aac104f5ad71a3316035ff6e07460533838afe07
mylinhpham <myylinhpham@gmail.com>
2022-03-06 23:03:08 -0500

    Initial commit to create the react project + included new depenencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from "./home";
+import About from "./backgroundInfo";
+import Courses from "./courses";
+import { TypographyStyle } from 'react-typography';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="navBar">
+            <div style={{ paddingRight: '10px', fontSize: '24px' }}><h1>UCI</h1></div>
+            <div style={{ fontSize: '18px', paddingTop: '50px'}}>{`Donald Bren School of \nInformation and Computer Science`}</div>
+        </div>
+          <Courses />
+          <About />
+        <Home />
     </div>
   );
 }
---
+++ src/courses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Courses() {
+    return(
+        <div>
+            <h1>Course Info</h1>
+        </div>
+    );
+};
+
+export default Courses;
